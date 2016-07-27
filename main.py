1374f2476f906a42cef2e8fcd6bbd412f2c45104
Dawson <dawsonmax10@gmail.com>
2016-07-27 19:58:24 +0000

    Heroku Commit

--- main.py
+++ main.py
@@ -3,41 +3,44 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+"""
 
-#  @app.route('/')
-#  def index():
-#      return "Method used: %s" %(request.method)
-#      #return "This is the homepage."
-#      
-#  @app.route('/about')
-#  def about():
-#      return '<h2>About</h2>'
-#      
-#      ba
-#  @app.route('/post/<int:post_id>')
-#  def post(post_id):
-#      return "<h2>Post id: %s" %(post_id)
-#  
-#  @app.route('/bacon', methods=['GET', 'POST'])
-#  def bacon():
-#      if request.method == 'POST':
-#          return 'You posted!'
-#      else:
-#          return 'You got me!'
-#  
-#  @app.route("/profile/<name>")
-#  def profile(name):
-#      return render_template("profile.html", name=name)
-#  
-#  @app.route('/')
-#  @app.route('/<user>')
-#  def index(user=None):
-#      return render_template("user.html", user=user)
-#
-#  @app.route('/shopping')
-#  def shopping():
-#      food = ["Orange Juice", "food!"]
-#      return render_template("shopping.html", food=food)
+  @app.route('/')
+  def index():
+      return "Method used: %s" %(request.method)
+      return "This is the homepage."
+      
+  @app.route('/about')
+  def about():
+      return '<h2>About</h2>'
+      
+      
+  @app.route('/post/<int:post_id>')
+  def post(post_id):
+      return "<h2>Post id: %s" %(post_id)
+  
+  @app.route('/bacon', methods=['GET', 'POST'])
+  def bacon():
+      if request.method == 'POST':
+          return 'You posted!'
+      else:
+          return 'You got me!'
+  
+  @app.route("/profile/<name>")
+  def profile(name):
+      return render_template("profile.html", name=name)
+  
+  @app.route('/')
+  @app.route('/<user>')
+  def index(user=None):
+      return render_template("user.html", user=user)
+
+  @app.route('/shopping')
+  def shopping():
+      food = ["Orange Juice", "food!"]
+      return render_template("shopping.html", food=food)
+
+"""
 
 @app.route('/')
 def index():
